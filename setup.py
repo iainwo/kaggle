26d026442eaf99e78c9b5595b696751f28ef4eef
Iain Wong <iainwong@outlook.com>
2020-02-08 20:42:22 -0500

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The challenge is to create a model that uses data from the first 24 hours of intensive care to predict patient survival. (Kaggle Proj) https://www.kaggle.com/c/widsdatathon2020/overview',
+    author='Iain Wong',
+    license='MIT',
+)
