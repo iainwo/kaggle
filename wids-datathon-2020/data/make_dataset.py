358b09c4b8160d38acc9b9aca9697b641adcfb06
Iain Wong <iainwong@outlook.com>
2020-02-08 21:45:04 -0500

    added prelim eda and preprocessing nb

--- wids-datathon-2020/data/make_dataset.py
+++ wids-datathon-2020/data/make_dataset.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
 import zipfile
+import pandas as pd
 
 
 @click.command()
@@ -20,9 +21,25 @@ def main(input_filepath, output_filepath, external_filepath):
 
     INPUT_FILEPATH = Path.cwd().joinpath(input_filepath)
     DATASET = Path.cwd().joinpath(external_filepath).joinpath('widsdatathon2020.zip')
+    
+    DATADICT_FILEPATH = Path.cwd().joinpath(input_filepath).joinpath('WiDS Datathon 2020 Dictionary.csv')
+    DATA = Path.cwd().joinpath(input_filepath).joinpath('training_v2.csv')
 
     with zipfile.ZipFile(DATASET, 'r') as zip_ref:
         zip_ref.extractall(INPUT_FILEPATH)
+        
+    datadict = pd.read_csv(DATADICT_FILEPATH)
+    continuous_cols = list(
+        list(datadict[datadict['Data Type'] == 'integer']['Variable Name'].unique())
+        + list(datadict[datadict['Data Type'] == 'numeric']['Variable Name'].unique())
+    )
+    categorical_cols = list(datadict[datadict['Data Type'] == 'string']['Variable Name'].unique())
+    binary_cols = list(datadict[datadict['Data Type'] == 'binary']['Variable Name'].unique())
+    
+    logger.info(f'Continuous cols: {continuous_cols}')
+    logger.info(f'Categorical cols: {categorical_cols}')
+    logger.info(f'Binary cols: {binary_cols}')
+    
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
