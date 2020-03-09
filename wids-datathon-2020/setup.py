bf0f2f552c3461d34184ca8bdef4d8841112325d
Iain Wong <iainwong@outlook.com>
2020-03-08 20:41:02 -0400

    added font resource to App

--- wids-datathon-2020/setup.py
+++ wids-datathon-2020/setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='wids-datathon-2020',
-    version='0.1.4',
+    version='0.1.5',
     license='MIT',
     author='Iain Wong',
     author_email='iainwong@outlook.com',
